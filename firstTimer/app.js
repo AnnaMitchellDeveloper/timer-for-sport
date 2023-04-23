7e9c1d10834999432ef10f25c6701e3855ce5c68
BarnesNoble <anna.mitchell.dev@gmail.com>
2023-04-23 15:36:02 -0400

    Added script in JS for countdown timers

---
+++ firstTimer/app.js
@@ -0,0 +1,29 @@
+const startButton = document.querySelector("#startButton");
+startButton.addEventListener("click", function () {
+  document.querySelector("#player").play();
+  document.querySelector("#myVideo").play();
+});
+
+let timerMinutes = 10;
+let timerSeconds = timerMinutes * 60;
+
+function calculateTime(){
+	const countdown = document.querySelector("#countdown");
+	let minutes = Math.floor(timerSeconds / 60);
+	let seconds = timerSeconds % 60;
+
+	if (seconds < 10) {
+		seconds = "0" + seconds;
+	}
+	countdown.textContent = `${minutes}:${seconds}`;
+	timerSeconds--;
+
+	if (timerSeconds < 0) {
+		stopTimer();
+		timerSeconds = 0;
+	}
+	function stopTimer(){
+		clearInterval(timerID);
+	}
+}
+let timerID = setInterval(calculateTime, 1000);
